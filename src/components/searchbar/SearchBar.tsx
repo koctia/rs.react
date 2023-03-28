ddda9e54c52bd91fde180b110ad4819a00ec7289
Konstantin Moskvin <mr.koctia@gmail.com>
2023-03-28 19:52:33 +0300

    feat: working with components in the search

--- src/components/searchbar/SearchBar.tsx
+++ src/components/searchbar/SearchBar.tsx
@@ -1,8 +1,59 @@
-import React, { ChangeEvent, Component } from 'react';
+import React, { ChangeEvent, Component, useState, useEffect, useMemo } from 'react';
 import { ISearchBar } from '../../interface/searchbar';
 import './searchbar.scss';
 
-class SearchBar extends Component<ISearchBar, { data: string }> {
+const SearchBar = () => {
+  const [dataValue, setDataValue] = useState(localStorage.getItem('rssearch') || '');
+  // const [dataEdit, setDataEdit] = useState(dataValue);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setDataValue(event.target.value);
+  };
+  console.log('dataValue', dataValue);
+
+  useEffect(() => {
+    // // setDataValue(localStorage.getItem('rssearch') || '');
+    // // const value = dataValue ? dataValue : '';
+    // // localStorage.setItem('rssearch', value);
+    // // console.log('componentDidMount');
+    // console.log('--->', dataValue);
+    // console.log('dataEdit', dataEdit);
+    // setDataEdit(dataValue);
+    const value = localStorage.getItem('rssearch');
+    if (value) setDataValue(dataValue);
+    return () => {
+      if (!dataValue) return;
+      const value = dataValue;
+      localStorage.setItem('rssearch', value);
+      // console.log('===>', dataValue);
+      // setDataEdit(dataValue);
+      // console.log('dataEdit', dataEdit);
+      // // console.log('componentWillMount');
+      // // console.log(dataValue !== localStorage.getItem('rssearch'));
+      // // if (dataValue !== localStorage.getItem('rssearch')) {
+      // //   const value = dataValue ? dataValue : '';
+      // //   localStorage.setItem('rssearch', value);
+      // // }
+    };
+    // }, []);
+  }, [dataValue]);
+
+  return (
+    <div className="main__search-input">
+      <input
+        className="main__search"
+        data-testid="search-input"
+        type="search"
+        placeholder="enter search..."
+        onChange={(event) => handleChange(event)}
+        // value={dataValue ? dataValue : ''}
+        value={dataValue}
+      />
+    </div>
+  );
+};
+
+class SearchBarOld extends Component<ISearchBar, { data: string }> {
   dataSearch(event: ChangeEvent) {
     const valueInput: Element = event.target;
     if (valueInput instanceof HTMLInputElement) {
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import {configDefaults} from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
