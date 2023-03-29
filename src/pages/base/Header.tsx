11fe79dc6629d841c7537a27657109024e61e35e
Konstantin Moskvin <mr.koctia@gmail.com>
2023-03-29 17:51:09 +0300

    feat: redesigned header component

--- src/pages/base/Header.tsx
+++ src/pages/base/Header.tsx
@@ -1,71 +1,63 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
-import { IHeaderName } from '../../interface/header';
 
 const setActiveLink = ({ isActive }: { isActive: boolean }) =>
   isActive ? 'header__active-link' : '';
 
-class Header extends Component<IHeaderName, { name: string }> {
-  constructor(props: IHeaderName) {
-    super(props);
-    this.state = {
-      name: 'Home',
-    };
-  }
+const Header = () => {
+  const [nameRouter, setNameRouter] = useState('Home');
 
-  componentDidMount() {
+  useEffect(() => {
     switch (location.pathname) {
       case '/':
-        this.setState({ name: 'Home' });
+        setNameRouter('Home');
         break;
       case '/about':
-        this.setState({ name: 'About' });
+        setNameRouter('About');
         break;
       case '/form':
-        this.setState({ name: 'Form' });
+        setNameRouter('Form');
         break;
       default:
-        this.setState({ name: 'Not Page' });
+        setNameRouter('Not Page');
     }
-  }
+  }, []);
 
-  render() {
-    return (
-      <header className="header">
-        <div className="header__container">
-          <div className="header__box">
-            <h2 className="header__header-name">{this.state.name}</h2>
-            <nav className="header__nav">
-              <NavLink
-                data-testid="home-link"
-                to="/"
-                onClick={() => this.setState({ name: 'Home' })}
-                className={setActiveLink}
-              >
-                Home
-              </NavLink>
-              <NavLink
-                data-testid="about-link"
-                to="/about"
-                onClick={() => this.setState({ name: 'About' })}
-                className={setActiveLink}
-              >
-                About
-              </NavLink>
-              <NavLink
-                data-testid="form-link"
-                to="/form"
-                onClick={() => this.setState({ name: 'Form' })}
-                className={setActiveLink}
-              >
-                Form
-              </NavLink>
-            </nav>
-          </div>
+  return (
+    <header className="header">
+      <div className="header__container">
+        <div className="header__box">
+          <h2 className="header__header-name">{nameRouter}</h2>
+          <nav className="header__nav">
+            <NavLink
+              data-testid="home-link"
+              to="/"
+              onClick={() => setNameRouter('Home')}
+              className={setActiveLink}
+            >
+              Home
+            </NavLink>
+            <NavLink
+              data-testid="about-link"
+              to="/about"
+              onClick={() => setNameRouter('About')}
+              className={setActiveLink}
+            >
+              About
+            </NavLink>
+            <NavLink
+              data-testid="form-link"
+              to="/form"
+              onClick={() => setNameRouter('Form')}
+              className={setActiveLink}
+            >
+              Form
+            </NavLink>
+          </nav>
         </div>
-      </header>
-    );
-  }
-}
+      </div>
+    </header>
+  );
+};
 
 export { Header };
