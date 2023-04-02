ffdc733a55b83a5df5d96a29fc7d44a3848bed12
Konstantin Moskvin <mr.koctia@gmail.com>
2023-04-02 03:21:49 +0300

    faet: add preloader

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -3,12 +3,16 @@ import { ICardData } from '../../interface/card';
 import './card.scss';
 
 const DEFAULT_IMAGE = 'https://stickerboom.ru/files/2012/11/26/678xfa73-300x215.png';
+const DEFAULT_URL = 'https://rs-react-api.vercel.app/';
 
 const Card = (props: ICardData) => {
   return (
     <div className="cards__box" data-testid="item-card">
       <div className="cards__url">
-        <img className="cards__img" src={props.url_l || DEFAULT_IMAGE} />
+        <img
+          className="cards__img"
+          src={(props.url_l && DEFAULT_URL + props.url_l) || DEFAULT_IMAGE}
+        />
       </div>
       <div className="cards__name">
         <div className="cards__first-name">{props.first_name}</div>
--- src/pages/main/Home.tsx
+++ src/pages/main/Home.tsx
@@ -1,17 +1,31 @@
 import React, { ChangeEvent, useEffect, useRef, useState } from 'react';
 import { SearchBar } from '../../components/searchbar/SearchBar';
 import { Card } from '../../components/card/Card';
-import MockData from '../../data/mock_data.json';
+// import MockData from '../../data/mock_data.json';
 import { ICardData } from '../../interface/card';
 import './home.scss';
 
 function Home() {
   const [dataValue, setDataValue] = useState(localStorage.getItem('rssearch') || '');
   const dataRef = useRef(dataValue);
+  const [catsData, setCatsData] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+
+  const fetchData = () => {
+    fetch('https://rs-react-api.vercel.app/cats')
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setCatsData(data);
+        setLoading(false);
+      });
+  };
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => setDataValue(event.target.value);
 
   useEffect(() => {
+    fetchData();
     dataRef.current = dataValue;
   }, [dataValue]);
 
@@ -23,6 +37,11 @@ function Home() {
 
   return (
     <>
+      {isLoading && (
+        <div className="loader-container">
+          <div className="spinner"></div>
+        </div>
+      )}
       <SearchBar
         id="search"
         type="search"
@@ -34,14 +53,16 @@ function Home() {
         The cards
       </h2>
       <div className="main__cards cards">
-        {MockData.filter((data: ICardData) => {
-          return (
-            data.first_name?.toLowerCase().includes(dataValue.toLowerCase()) ||
-            data.last_name?.toLowerCase().includes(dataValue.toLowerCase())
-          );
-        }).map((data: ICardData) => {
-          return <Card key={data.id} {...data} />;
-        })}
+        {catsData
+          .filter((data: ICardData) => {
+            return (
+              data.first_name?.toLowerCase().includes(dataValue.toLowerCase()) ||
+              data.last_name?.toLowerCase().includes(dataValue.toLowerCase())
+            );
+          })
+          .map((data: ICardData) => {
+            return <Card key={data.id} {...data} />;
+          })}
       </div>
     </>
   );
