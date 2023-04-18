bdb2018dc6174156088e14e5553f1d1fccc47e09
Konstantin Moskvin <mr.koctia@gmail.com>
2023-04-18 18:03:50 +0300

    feat: edit config file

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,12 @@ import { configDefaults } from 'vitest/config';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build',
+  },
+  server: {
+    open: true,
+  },
   test: {
     globals: true,
     environment: 'jsdom',
